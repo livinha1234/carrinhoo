64a8756ee07710a700a53ec2cdf18db4dab80ffd
GuiMartos <guilhermemartos2005@gmail.com>
2023-08-11 12:15:29 -0300

    Adicionando imagens e mudando o "StatusBar"

--- App.tsx
+++ App.tsx
@@ -1,21 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Carrinho from './src/pages/Carrinho';
+import { View, StatusBar, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView>
+      <StatusBar/>
       <Carrinho/>
-      <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
