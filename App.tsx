1e6d7de23aa155bc0d75c050e39bbed2211fba37
GuiMartos <guilhermemartos2005@gmail.com>
2023-08-11 11:56:04 -0300

    Criando o componente "Carrinho"

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Carrinho from './src/pages/Carrinho';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Word!!</Text>
+      <Carrinho/>
       <StatusBar style="auto" />
     </View>
   );
