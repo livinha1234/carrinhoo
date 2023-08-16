ca89a5cbaf55bd53fb1f38a3949540c316c6c35d
livinha1234 <liviamariasouza268@gmail.com>
2023-08-16 15:40:43 -0300

    carrinho

--- src/pages/Carrinho/index.tsx
+++ src/pages/Carrinho/index.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { StyleSheet,Image, Dimensions, Text, View } from 'react-native';
+
+
 import topo from '../../../assets/topo.png'
 
+const width = Dimensions.get('screen').width;
+
 export default function Carrinho (){
-    return <Image source={topo}/>
+    return <>
+    <Image source={topo} style={estilos.topo}/>
+    <Text style={estilos.titulo}>Detalhe do carrinho</Text>
+    <View>
+      <Text>Carrinho de Compras</Text>
+      <Text>Fazenda Etec Cidade do Livro</Text>
+      <Text>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha </Text>
+      <Text>
+        R$ 40,00
+      </Text>
+    </View>
+    </>
 }
+
+const estilos = StyleSheet.create({
+topo:{
+  width: "100%",
+  height: 578 / 768 * width
+},
+titulo:{
+  fontSize:24,
+  fontWeight:"bold",
+  lineHeight:26,
+  width:"100%",
+  textAlign:"center",
+  color: "white",
+  position: "absolute",
+  padding: 16
+}
+})
